bfc7e7506adaf3f46f02d8b76c78e77e05e552f7
jesussantana <jesus@esmarketingdigital.com>
2022-08-30 08:32:45 +0200

    Project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This analysis is for case study 1 from the Google Data Analytics Certificate (Cyclistic)',
+    author='Jesus Santana',
+    license='MIT',
+)
